62d92894c1d0b17ed4617ea6e4d57ce91a5717b9
Mohammed Alqmase <alqumasi@gmail.com>
2025-08-24 09:36:28 +0300

    create complaints table

---
+++ database/migrations/2025_08_24_063246_create_complaints_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // https://judg.moj.gov.ye/
+        Schema::create('complaints', function (Blueprint $table) {
+            $table->id();
+            // موضوع الشكوى
+            $table->string('complaint_subject');
+            // تفاصيل الشكوى
+            $table->text('complaint_details');
+            // تاريخ الشكوى
+            $table->date('complaint_date');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('complaints');
+    }
+};
